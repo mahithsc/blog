84199c3f0895d3f809591d3093e34e398dd58170
mahithsc <mahith.chitrapu@gmail.com>
2023-08-24 21:36:07 -0400

    adding link to google docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,60 +4,58 @@ import { AsciiRenderer, OrbitControls } from '@react-three/drei'
 
 
 function Box(props: any) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref:any = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((_state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(_event) => click(!clicked)}
-      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-      onPointerOut={(_event) => hover(false)}>
-      <torusGeometry args={[10, 3, 16, 100]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
+    // This reference gives us direct access to the THREE.Mesh object
+    const ref: any = useRef()
+    // Hold state for hovered and clicked events
+    const [hovered, hover] = useState(false)
+    const [clicked, click] = useState(false)
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((_state, delta) => (ref.current.rotation.x += delta))
+    // Return the view, these are regular Threejs elements expressed in JSX
+    return (
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onClick={(_event) => click(!clicked)}
+            onPointerOver={(event) => (event.stopPropagation(), hover(true))}
+            onPointerOut={(_event) => hover(false)}>
+            <torusGeometry args={[10, 3, 16, 100]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
 }
 
 const Index = () => {
 
-  const [invert, _setInvert] = useState<boolean>(false)
-  return (
+    const [invert, _setInvert] = useState<boolean>(false)
+    return (
 
-    <div className="h-screen">
-      <div className='bg-black text-white mt-5 absolute top-0 z-10 bg-opacity-50 w-[80%] h-14 flex items-center justify-evenly rounded-md left-0 right-0 mx-auto shadow-xl'>
-        <div className='font-bold text-xl'>Mahith Chitrapu</div>
-        <a>blog</a>
-        <a>resume</a>
-        <a>GitHub</a>
-        <a>LinkedIn</a>
-      </div>
-      <Canvas style={{ width: '100%', height: '100%', fontWeight:'bold'}}>
-        <AsciiRenderer 
-        characters=' .:-+*=%@#'
-        fgColor='black'
-        invert={invert}
-        bgColor='white'
-
-        />
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-        <OrbitControls />
-      </Canvas>
-    </div>
-  )
+        <div className="h-screen">
+            <div className='bg-black text-white mt-5 absolute top-0 z-10 bg-opacity-50 w-[80%] h-14 flex items-center justify-evenly rounded-md left-0 right-0 mx-auto shadow-xl'>
+                <div className='font-bold text-xl'>Mahith Chitrapu</div>
+                <a>blog</a>
+                <a href='https://docs.google.com/document/d/1YajIPifoXNTf7gQac1x3pS21Q7--Vta5x_yeAS0tlXo/edit?usp=sharing'>resume</a>
+                <a>GitHub</a>
+                <a>LinkedIn</a>
+            </div>
+            <Canvas style={{ width: '100%', height: '100%', fontWeight: 'bold' }}>
+                <AsciiRenderer
+                    characters=' .:-+*=%@#'
+                    fgColor='black'
+                    invert={invert}
+                    bgColor='white'
+                />
+                <ambientLight intensity={0.5} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                <pointLight position={[-10, -10, -10]} />
+                <Box position={[-1.2, 0, 0]} />
+                <Box position={[1.2, 0, 0]} />
+                <OrbitControls />
+            </Canvas>
+        </div>
+    )
 }
 
 export default Index
 
-      
