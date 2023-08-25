ae61c0cccd2476d47c957d8c9e06de731a419401
mahithsc <mahith.chitrapu@gmail.com>
2023-08-24 21:52:07 -0400

    animation for the website titles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { AsciiRenderer, OrbitControls } from '@react-three/drei'
+import { motion } from 'framer-motion'
 
 
 function Box(props: any) {
@@ -32,13 +33,81 @@ const Index = () => {
     return (
 
         <div className="h-screen">
-            <div className='bg-black text-white mt-5 absolute top-0 z-10 bg-opacity-50 w-[80%] h-14 flex items-center justify-evenly rounded-md left-0 right-0 mx-auto shadow-xl'>
-                <div className='font-bold text-xl'>Mahith Chitrapu</div>
-                <a>blog</a>
-                <a href='https://docs.google.com/document/d/1YajIPifoXNTf7gQac1x3pS21Q7--Vta5x_yeAS0tlXo/edit?usp=sharing'>resume</a>
-                <a href='https://github.com/mahithsc'>GitHub</a>
-                <a href='https://www.linkedin.com/in/mchitrapu/'>LinkedIn</a>
-            </div>
+            <motion.div
+                className='bg-black text-white mt-5 absolute top-0 z-10 bg-opacity-50 w-[80%] h-14 flex items-center justify-evenly rounded-md left-0 right-0 mx-auto shadow-xl'
+                variants={{
+                    hidden: { opacity: 0, y: 10 },
+                    visible: { opacity: 1, y: 0 }
+                }}
+                initial="hidden"
+                animate="visible"
+                transition={{ duration: 0.5 }}
+            >
+                <motion.div
+                    className='font-bold text-xl'
+                    variants={{
+                        hidden: { opacity: 0, y: 10 },
+                        visible: { opacity: 1, y: 0 }
+                    }}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ duration: 0.5, delay: 0.5 }}
+                >
+                    Mahith Chitrapu
+                </motion.div>
+                
+                <motion.a
+                    variants={{
+                        hidden: { opacity: 0, y: 10 },
+                        visible: { opacity: 1, y: 0 }
+                    }}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ duration: 0.5, delay: 1 }}
+                >
+                    blog
+                </motion.a>
+                
+                <motion.a
+                    href='https://docs.google.com/document/d/1YajIPifoXNTf7gQac1x3pS21Q7--Vta5x_yeAS0tlXo/edit?usp=sharing'
+                    variants={{
+                        hidden: { opacity: 0, y: 10 },
+                        visible: { opacity: 1, y: 0 }
+                    }}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ duration: 0.5, delay: 1.5 }}
+                >
+                    resume
+                </motion.a>
+                
+                <motion.a 
+                    href='https://github.com/mahithsc'
+                    variants={{
+                        hidden: { opacity: 0, y: 10 },
+                        visible: { opacity: 1, y: 0 }
+                    }}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ duration: 0.5, delay: 2 }}
+                >
+
+                    GitHub
+                </motion.a>
+
+                <motion.a 
+                    href='https://www.linkedin.com/in/mchitrapu/'
+                    variants={{
+                        hidden: { opacity: 0, y: 10 },
+                        visible: { opacity: 1, y: 0 }
+                    }}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ duration: 0.5, delay: 2.5 }}
+                >
+                    LinkedIn
+                </motion.a>
+            </motion.div>
             <Canvas style={{ width: '100%', height: '100%', fontWeight: 'bold' }}>
                 <AsciiRenderer
                     characters=' .:-+*=%@#'
