98efcd16474faf9faaa2b9f8f7e539c61364a140
mahithsc <mahith.chitrapu@gmail.com>
2023-08-24 22:30:59 -0400

    removing clicked and not clicked state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,21 +8,22 @@ function Box(props: any) {
     // This reference gives us direct access to the THREE.Mesh object
     const ref: any = useRef()
     // Hold state for hovered and clicked events
-    const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
+    // const [hovered, hover] = useState(false)
+    // const [clicked, click] = useState(false)
     // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((_state, delta) => (ref.current.rotation.x += delta))
+    useFrame((_state, delta) => (ref.current.rotation.y += delta))
     // Return the view, these are regular Threejs elements expressed in JSX
     return (
         <mesh
             {...props}
             ref={ref}
-            scale={clicked ? 1.5 : 1}
-            onClick={(_event) => click(!clicked)}
-            onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-            onPointerOut={(_event) => hover(false)}>
+            // scale={clicked ? 1.5 : 1}
+            // onClick={(_event) => click(!clicked)}
+            // onPointerOver={(event) => (event.stopPropagation(), hover(true))}
+            // onPointerOut={(_event) => hover(false)}
+            >
             <torusGeometry args={[10, 3, 16, 100]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            <meshStandardMaterial color='orange' />
         </mesh>
     )
 }
@@ -119,8 +120,8 @@ const Index = () => {
                 <ambientLight intensity={0.5} />
                 <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
                 <pointLight position={[-10, -10, -10]} />
-                <Box position={[-1.2, 0, 0]} />
-                <Box position={[1.2, 0, 0]} />
+                {/* <Box position={[-1.2, 0, 0]} /> */}
+                <Box position={[0, 0, 0]} />
                 <OrbitControls />
             </Canvas>
         </div>
