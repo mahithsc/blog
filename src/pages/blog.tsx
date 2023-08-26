043229f33d3d05d039ed7620cebccc8a5dae3513
mahithsc <mahith.chitrapu@gmail.com>
2023-08-26 09:25:28 -0400

    adding a blog section

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,26 +1,13 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
+
+import BlogBlock from '../components/BlogBlock'
+import '../index.css'
 
 const Blog = () => {
-    const markdown = `A paragraph with *emphasis* and **strong importance**.
-
-> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
-
-* Lists
-* [ ] todo
-* [x] done
-
-A table:
-
-| a | b |
-| - | - |
-`
 
     return (
         <div className='mt-28 w-[100%] max-w-6xl mx-auto'>
-            {/* <div className='text-3xl font-bold mx-5'>blog</div> */}
-            <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+
+            <BlogBlock/>
         </div>
     )
 }
